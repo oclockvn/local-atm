3967810dffb21b8b36f22075dd727b0e0e1f8198
Quang Phan <oclockvn@gmail.com>
2024-01-19 14:17:01 +0700

    add dashboard form

--- LocalAtm/DashboardForm.Designer.cs
+++ LocalAtm/DashboardForm.Designer.cs
@@ -28,12 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "DashboardForm";
+            btnViewBalance = new Button();
+            btnWidthdraw = new Button();
+            btnViewTransactions = new Button();
+            lblUsername = new Label();
+            SuspendLayout();
+            // 
+            // btnViewBalance
+            // 
+            btnViewBalance.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            btnViewBalance.Location = new Point(21, 156);
+            btnViewBalance.Name = "btnViewBalance";
+            btnViewBalance.Size = new Size(448, 34);
+            btnViewBalance.TabIndex = 0;
+            btnViewBalance.Text = "View Balance";
+            btnViewBalance.UseVisualStyleBackColor = true;
+            // 
+            // btnWidthdraw
+            // 
+            btnWidthdraw.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            btnWidthdraw.Location = new Point(21, 208);
+            btnWidthdraw.Name = "btnWidthdraw";
+            btnWidthdraw.Size = new Size(448, 34);
+            btnWidthdraw.TabIndex = 1;
+            btnWidthdraw.Text = "Widthdraw";
+            btnWidthdraw.UseVisualStyleBackColor = true;
+            // 
+            // btnViewTransactions
+            // 
+            btnViewTransactions.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            btnViewTransactions.Location = new Point(21, 260);
+            btnViewTransactions.Name = "btnViewTransactions";
+            btnViewTransactions.Size = new Size(448, 34);
+            btnViewTransactions.TabIndex = 2;
+            btnViewTransactions.Text = "View Last Transactions";
+            btnViewTransactions.UseVisualStyleBackColor = true;
+            // 
+            // lblUsername
+            // 
+            lblUsername.AutoSize = true;
+            lblUsername.Location = new Point(12, 9);
+            lblUsername.Name = "lblUsername";
+            lblUsername.Size = new Size(33, 25);
+            lblUsername.TabIndex = 3;
+            lblUsername.Text = "Hi,";
+            // 
+            // DashboardForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(481, 450);
+            Controls.Add(lblUsername);
+            Controls.Add(btnViewTransactions);
+            Controls.Add(btnWidthdraw);
+            Controls.Add(btnViewBalance);
+            Name = "DashboardForm";
+            Text = "Dashboard | ATM version 1.0";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnViewBalance;
+        private Button btnWidthdraw;
+        private Button btnViewTransactions;
+        private Label lblUsername;
     }
 }
--- LocalAtm/LoginForm.cs
+++ LocalAtm/LoginForm.cs
@@ -16,5 +16,10 @@ namespace LocalAtm
         {
             InitializeComponent();
         }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            new DashboardForm().ShowDialog();
+        }
     }
 }
